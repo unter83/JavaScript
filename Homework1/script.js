79eb257dd08a8e3146984a02e5d38d93355c2e2c
unter <unter83@gmail.com>
2023-12-20 20:10:27 +0300

    first commit

---
+++ Homework1/script.js
@@ -0,0 +1,17 @@
+
+console.log(`Задание 1
+`);
+const celsius = 36.6;
+
+let fahrenheit = celsius * 1.8 + 32;
+
+console.log(`Цельсий = ${celsius}
+Фаренгейт = ${fahrenheit}`);
+console.log(``);
+console.log(`Задание 2
+`);
+
+const name = "Victor";
+let admin = name;
+console.log(`name = ${name}`);
+console.log(`admin = ${admin}`);
